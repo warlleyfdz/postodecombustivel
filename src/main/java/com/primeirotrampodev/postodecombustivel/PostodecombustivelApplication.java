6bcafa7931aa5c64b17ebd48a8f95feb51694ef9
warlleyfdz <warlleyfdz@hotmail.com>
2023-03-07 21:23:00 -0300

    Add Primeiro Commit

---
+++ src/main/java/com/primeirotrampodev/postodecombustivel/PostodecombustivelApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.postodecombustivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostodecombustivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostodecombustivelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/postodecombustivel/PostodecombustivelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.postodecombustivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostodecombustivelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
